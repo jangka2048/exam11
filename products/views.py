84ab3a62ab5421f01e3f02c80dbea3d07a931125
jhs512 <jangka512@gmail.com>
2021-12-22 21:00:44 +0900

    질문등록 후 원래페이지로 이동

--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
+from django.contrib.contenttypes.models import ContentType
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from products.models import Product
+from qna.models import Question
 
 
 def product_list(request: HttpRequest):
@@ -25,6 +27,13 @@ def product_detail(request: HttpRequest, id):
 
 def question_create(request: HttpRequest, id):
     product = get_object_or_404(Product, id=id)
+
+    if request.method == "POST":
+        product_content_type = ContentType.objects.get_for_model(product)
+        body = request.POST.get("body")
+        question = Question(user_id=1, content_type=product_content_type, object_id=product.id, body=body)
+        question.save()
+        return redirect("products:detail", id=product.id)
 
     product_reals = product.product_reals.order_by('option_1_display_name', 'option_2_display_name')
 
