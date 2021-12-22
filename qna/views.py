ca1f2eb3c292bc20d139a216c7e8f930d78226e6
jangka2048 <jangka2048@gmail.com>
2021-12-22 13:38:07 +0900

    일단 질문이 저장되긴 함

--- qna/views.py
+++ qna/views.py
@@ -1,6 +1,16 @@
+from django.contrib.contenttypes.models import ContentType
 from django.http import HttpRequest, HttpResponse
 
-
 # Create your views here.
+from products.models import Product
+from qna.models import Question
+
+
 def create_question(request: HttpRequest):
-    return HttpResponse("안녕")
+    product_id = 1
+    product = Product.objects.get(id=product_id)
+    product_content_type = ContentType.objects.get_for_model(product)
+    body = request.POST.get("body")
+    question = Question(user_id=1, content_type=product_content_type, object_id=product_id, body=body)
+    question.save()
+    return HttpResponse("성공")
