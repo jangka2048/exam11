a2e4d59752e658224a7f7739fd654759bfcba124
jhs512 <jangka512@gmail.com>
2021-12-22 20:55:26 +0900

    상품질문작성URL을 변경

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ app_name = 'products'
 urlpatterns = [
     path('', views.product_list, name='list'),
     path('<int:id>/', views.product_detail, name='detail'),
+    path('<int:id>/question/create/', views.question_create, name='question_create'),
 ]
