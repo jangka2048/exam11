2d2faeace5c3171c34beb44c3300999449ef6dde
jangka2048 <jangka2048@gmail.com>
2021-12-22 13:33:09 +0900

    질문 폼 생성

--- qna/views.py
+++ qna/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
 
 # Create your views here.
+def create_question(request: HttpRequest):
+    return HttpResponse("안녕")
