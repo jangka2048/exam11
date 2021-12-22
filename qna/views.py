08753f2ef612c2f78807385bd6285ef9e5d925c0
jangka2048 <jangka2048@gmail.com>
2021-12-22 13:43:06 +0900

    선택된 상품에 관련된 질문이 생성

--- qna/views.py
+++ qna/views.py
@@ -6,11 +6,12 @@ from products.models import Product
 from qna.models import Question
 
 
-def create_question(request: HttpRequest):
-    product_id = 1
-    product = Product.objects.get(id=product_id)
-    product_content_type = ContentType.objects.get_for_model(product)
-    body = request.POST.get("body")
-    question = Question(user_id=1, content_type=product_content_type, object_id=product_id, body=body)
-    question.save()
+def create_question(request: HttpRequest, content_type, object_id):
+    product_id = object_id
+    if content_type == "product":
+        product = Product.objects.get(id=product_id)
+        product_content_type = ContentType.objects.get_for_model(product)
+        body = request.POST.get("body")
+        question = Question(user_id=1, content_type=product_content_type, object_id=product_id, body=body)
+        question.save()
     return HttpResponse("성공")
