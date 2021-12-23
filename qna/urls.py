63d00bff612542c34332f5245534067c96cf27a3
jangka2048 <jangka2048@gmail.com>
2021-12-23 13:34:16 +0900

    질문 작성시 로그인 한 사용자를 작성자로

--- qna/urls.py
+++ qna/urls.py
@@ -1,9 +1,5 @@
-from django.urls import path
-
-from qna import views
-
 app_name = 'qna'
 
 urlpatterns = [
-    path('create_question/<str:content_type>/<int:object_id>/', views.create_question, name='create_question')
+
 ]
