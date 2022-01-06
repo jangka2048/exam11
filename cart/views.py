19af23c4496864f58a972c7d03d73f5b1a244a8b
jhs512 <jangka512@gmail.com>
2022-01-06 11:00:12 +0900

    장바구니 리스트

--- cart/views.py
+++ cart/views.py
@@ -3,10 +3,11 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest
 
 # Create your views here.
-from django.shortcuts import redirect
-from django.views.decorators.http import require_POST
+from django.shortcuts import redirect, render
+from django.views.decorators.http import require_POST, require_GET
 
 from cart.forms import ProductCartAddForm
+from cart.models import CartItem
 from products.models import ProductReal
 
 
@@ -26,3 +27,13 @@ def add(request: HttpRequest):
 
     messages.error(request, form['quantity'].errors, 'danger')
     return redirect('products:detail', product_real.product.id)
+
+
+@login_required
+@require_GET
+def list(request: HttpRequest):
+    cart_items = CartItem.objects.filter(user=request.user)
+
+    return render(request, "cart/list.html", {
+        "cart_items": cart_items
+    })
